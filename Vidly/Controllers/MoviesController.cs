06d06d8264463f3702f7b7bb11601ef7fd1bcf5a
Anirudh Deshpande <anirudh.deshpande1@gmail.com>
2020-07-31 19:09:41 -0600

    Add index action and by released date action to movies controller

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -7,6 +7,7 @@ using Vidly.Models;
 
 namespace Vidly.Controllers
 {
+    [RoutePrefix("movies")]
     public class MoviesController : Controller
     {
         // GET: Movies/Random
@@ -14,6 +15,31 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() {Name = "Shrek"};
             return View(movie);
+            
         }
+        
+        [Route("released/{year:regex(\\d{4})}/{month:range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" +month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex, sortBy));
+        }
+
     }
 }
