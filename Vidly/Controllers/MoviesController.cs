f2e3ac3f69cd56dc4f601934fa6fcc8fce891b4b
Anirudh Deshpande <anirudh.deshpande1@gmail.com>
2020-08-04 00:12:38 -0600

    Update bootstrap theme to default; Make navbar into a partial view and add movies index view and view model

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -14,7 +15,20 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek"};
-            return View(movie);
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
             
         }
         
@@ -30,15 +44,20 @@ namespace Vidly.Controllers
         }
 
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            List<Movie> movies = new List<Movie>
+                {
+                    new Movie { Name = "Shrek" },
+                    new Movie { Name = "Wall-E" }
+                };
 
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+            var viewModel = new MoviesViewModel
+            {
+                Movies = movies
+            };
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex, sortBy));
+            return View(viewModel);
         }
 
     }
